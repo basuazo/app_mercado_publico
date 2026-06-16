407e96e15e4c193f29d3e8f1eecb5d73dea464f1
Boris Suazo <borisanibalsuazo@gmail.com>
2026-06-16 09:28:35 -0400

    fix deploy

--- app/api/main.py
+++ app/api/main.py
@@ -118,3 +118,7 @@ def _make_app() -> FastAPI:
     s = get_settings()
     e = make_engine(s)
     return create_app(s, e)
+
+
+# Instancia de módulo requerida por: uvicorn app.api.main:app
+app = _make_app()
